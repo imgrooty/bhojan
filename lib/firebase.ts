1ae30d1f82ebf8c40ce43f0b2ebb52ffb8fdbefd
Bikram Sharma <us08692@gmail.com>
2025-06-10 18:50:38 +0545

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
-  // ...other config
+
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
